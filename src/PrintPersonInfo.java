c6aaa11fcc2634cd5e54b37c6948939f8c998e7b
colem <cole.miller@csuglobal.edu>
2024-06-16 18:31:14 -0600

    Initial Commit with PrintPersonInfo.java

---
+++ src/PrintPersonInfo.java
@@ -0,0 +1,35 @@
+/* ------------------------------------------------------------------------------------
+Program Name: CSC320_CTA1_Option1.java
+Author: Cole Miller
+Date: 06/16/24
+---------------------------------------------------------------------------------------
+Psuedocode:
+    1. Start
+    2. Declare variables for first name, last name, street address, city, and zip code.
+    3. Initialize variables with the appropriate values.
+    4. Print each variable on a new line.
+    5. End
+---------------------------------------------------------------------------------------
+Program Inputs: None
+Program Outputs: Display first name, last name, street address, city, and zip code.
+---------------------------------------------------------------------------------------
+ */
+public class PrintPersonInfo {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        String firstName = "Cole";
+        String lastName = "Miller";
+        String streetAddress = "123 Park Ave";
+        String city = "Fairytale";
+        String zipCode = "12345";
+
+        // Print the info
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+
+
+}
